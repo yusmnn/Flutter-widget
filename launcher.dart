8e20626700d2a14d76ee3bd05a87219364ee19ee
Rahul Verdi <46898468+Rhlverdi@users.noreply.github.com>
2022-10-05 15:49:39 +0800

    add file for hacktoberfest

---
+++ launcher.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: unnecessary_new, prefer_const_constructors
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:olshop/constans.dart';
+import 'package:olshop/users/loginPage.dart' as login;
+
+class LauncherPage extends StatefulWidget {
+  const LauncherPage({Key? key}) : super(key: key);
+
+  @override
+  State<LauncherPage> createState() => _LauncherPageState();
+}
+
+class _LauncherPageState extends State<LauncherPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => login.LoginPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(0)),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.grey.shade200,
+                offset: Offset(2, 4),
+                blurRadius: 5,
+                spreadRadius: 2)
+          ],
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Palette.bg1, Palette.bg2],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Center(
+              child: new Image.asset(
+                "assets/logo.png",
+                height: 90.0,
+                width: 270.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
